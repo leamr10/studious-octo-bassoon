6be012d18342cd28a8ac6c02e3662b93132d73a3
leamr10 <leamr10@gmail.com>
2023-06-07 20:21:39 -0400

    typed out questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,26 +19,33 @@ var questions = [
         answer: "3. alerts"
     },
     {   
-    question: "Commonly used data types DO Not Include:",
-    choices: [
-        "1. strings", "2. booleans", "3. alerts", "4. numbers"
+        question: "The condition in an if/else statement is enclosed with _______. ",
+        choices: [
+            "1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"
     ],
-    answer: "3. alerts"
-},
-{   
-    question: "Commonly used data types DO Not Include:",
-    choices: [
-        "1. strings", "2. booleans", "3. alerts", "4. numbers"
+        answer: "3. parenthesis"
+    },
+    {   
+        question: "Arrays in JavaScript can be used to store ________. ",
+        choices: [
+            "1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"
     ],
-    answer: "3. alerts"
-},
-{   
-    question: "Commonly used data types DO Not Include:",
-    choices: [
-        "1. strings", "2. booleans", "3. alerts", "4. numbers"
+        answer: "4. all of the above"
+    },
+    {   
+        question: "String values must be enclosed within ________ when being assigned to variables. ",
+        choices: [
+           "1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"
     ],
-    answer: "3. alerts"
+        answer: "3. quotes"
 }
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: "
+        choices: [
+            "1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"
+        ]
+        answer: "4. console log"
+    }
 ];
 
 // this variable is used to keep track of what question the user is on
